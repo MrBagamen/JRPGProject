df234ddf61ceb455071444c347cf144aefa9d33d
Mika Attila <radiantstatue@gmail.com>
2013-09-02 23:14:24 +0200

    Lel remove unused import

--- src/jrpgproject/JRPGProject.java
+++ src/jrpgproject/JRPGProject.java
@@ -3,7 +3,6 @@ package jrpgproject;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.GL11;
 import static org.lwjgl.opengl.GL11.*;
 
 public class JRPGProject
